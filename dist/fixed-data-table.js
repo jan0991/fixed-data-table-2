d1581d1c77e7e9388190fad3731f9d0ead79447e
Jan <janincadez@gmail.com>
2018-03-22 19:47:09 +0100

    git push

--- dist/fixed-data-table.js
+++ dist/fixed-data-table.js
@@ -7033,8 +7033,8 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var MOVE_AMPLITUDE = 1.6;
-var DECELERATION_AMPLITUDE = 1.6;
+var MOVE_AMPLITUDE = 1.2;
+var DECELERATION_AMPLITUDE = 1.2;
 var DECELERATION_FACTOR = 325;
 var TRACKER_TIMEOUT = 100;
 
