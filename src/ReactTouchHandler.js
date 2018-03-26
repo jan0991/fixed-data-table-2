dfdc57bc7460a1e2e26edceeced0789bb6f8b81e
Jan <janincadez@gmail.com>
2018-03-26 17:19:04 +0200

    .

--- src/ReactTouchHandler.js
+++ src/ReactTouchHandler.js
@@ -22,7 +22,7 @@ import requestAnimationFramePolyfill from 'requestAnimationFramePolyfill';
 var MOVE_AMPLITUDE = 1.2;
 var DECELERATION_AMPLITUDE = 1.2;
 var DECELERATION_FACTOR = 325;
-var TRACKER_TIMEOUT = 0;
+var TRACKER_TIMEOUT = 100;
 
 class ReactTouchHandler {
   /**
