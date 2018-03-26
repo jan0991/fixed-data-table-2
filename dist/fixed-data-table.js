3951b877aa6dbbdd6fdd28e1e7a13ace98bceabf
Jan <janincadez@gmail.com>
2018-03-26 18:20:54 +0200

    .

--- dist/fixed-data-table.js
+++ dist/fixed-data-table.js
@@ -7033,9 +7033,9 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var MOVE_AMPLITUDE = 1.2;
-var DECELERATION_AMPLITUDE = 1.2;
-var DECELERATION_FACTOR = 325;
+var MOVE_AMPLITUDE = 1.0;
+var DECELERATION_AMPLITUDE = 1.0;
+var DECELERATION_FACTOR = 0;
 var TRACKER_TIMEOUT = 0;
 
 var ReactTouchHandler = function () {
