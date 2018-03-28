8d1f91a5b96b173e4223aa28c04ddc0d5adfc64a
jan0991 <janincadez@gmail.com>
2018-03-28 22:27:10 +0200

    Update ReactTouchHandler.js

--- internal/ReactTouchHandler.js
+++ internal/ReactTouchHandler.js
@@ -30,9 +30,9 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var MOVE_AMPLITUDE = 1.4;
-var DECELERATION_AMPLITUDE = 1.4;
-var DECELERATION_FACTOR = 100;
+var MOVE_AMPLITUDE = 1.6;
+var DECELERATION_AMPLITUDE = 1.6;
+var DECELERATION_FACTOR = 325;
 var TRACKER_TIMEOUT = 100;
 
 var ReactTouchHandler = function () {
